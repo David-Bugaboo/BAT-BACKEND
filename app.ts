6f2e6a0f9d768dbe9e3043439b42b3f15d7dda07
David Faco <david.faco@bugaboostudio.com>
2023-10-30 13:02:54 -0300

    fix: error handling on token

--- app.ts
+++ app.ts
@@ -39,7 +39,7 @@ app.use("/api", async (req:any, res:Response, next:NextFunction) =>
 
         var token = authHeader && authHeader.split(' ')[1]
 
-        if (token == null) {
+        if (!token) {
             
             return res.json({"message":"no JWT token present"}).status(401);
             
